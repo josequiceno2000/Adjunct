03eda025008eacd28ba874e41aacb5ff351b096f
Jose <divina.familia.nostri@gmail.com>
2025-10-31 14:33:23 -0500

    allow user input

--- adjunct/main.py
+++ adjunct/main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -6,9 +7,11 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
+contents = sys.argv[1:] if len(sys.argv) > 1 else print("Please provide input text as command line arguments.")
+
 response = client.models.generate_content(
     model="gemini-2.0-flash-001", 
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    contents=contents)
 prompt_tokens = response.usage_metadata.prompt_token_count
 response_tokens = response.usage_metadata.candidates_token_count
 
