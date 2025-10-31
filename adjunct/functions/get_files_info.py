fae09d63d7d2729483bfa8d0dcd50ad0bc3974c5
Jose <divina.familia.nostri@gmail.com>
2025-10-31 16:17:56 -0500

    test(functions) add tests

--- adjunct/functions/get_files_info.py
+++ adjunct/functions/get_files_info.py
@@ -1,16 +1,30 @@
 import os
+from pathlib import Path
 
 def get_files_info(working_directory, directory="."):
-    if not os.path.isabs(directory):
+    working_path_abs = Path(working_directory).resolve()
+    target_path_abs = Path(working_directory, directory).resolve()
+    
+    if not (str(target_path_abs).startswith(str(working_path_abs))):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    if not os.path.exists(os.path.join(working_directory, directory)):
+
+    if not target_path_abs.is_dir():
         return f'Error: "{directory}" is not a directory'
     
-    full_path = os.path.join(working_directory, directory)
+    directory_name = target_path_abs.name
+    
+    files_list = [f"Result for {directory_name}:"]
 
-    for file in os.listdir(full_path):
-        file_path = os.path.join(full_path, file)
-        file_size = os.path.get_size(file_path)
-        is_dir = os.path.isdir(file_path)
+    try:
+        for file in os.listdir(target_path_abs):
+            file_path = target_path_abs / file
+            file_size = os.path.getsize(file_path)
+            is_dir = os.path.isdir(file_path)
 
-        print(f"{file}: file_size={file_size} bytes, is_dir={is_dir}")
+            files_list.append(f"- {file}: file_size={file_size} bytes, is_dir={is_dir}")
+        
+        return "\n".join(files_list) if files_list else "Directory is empty."
+        
+    
+    except Exception as e:
+        return f"Error: {str(e)}"
