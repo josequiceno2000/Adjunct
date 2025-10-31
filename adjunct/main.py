7a770ffb074044d01a83f22a9af50427e7a32cd1
Jose <divina.familia.nostri@gmail.com>
2025-10-31 14:39:40 -0500

    feature(main): keep running messages log

--- adjunct/main.py
+++ adjunct/main.py
@@ -2,18 +2,24 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-contents = sys.argv[1:] if len(sys.argv) > 1 else print("Please provide input text as command line arguments.")
+user_prompt = sys.argv[1:] if len(sys.argv) > 1 else print("Please provide input text as command line arguments.")
+
+messages = [
+    types.Content(role="user",parts=[types.Part(text=user_prompt)]),
+]
 
 response = client.models.generate_content(
     model="gemini-2.0-flash-001", 
-    contents=contents)
+    contents=messages)
 prompt_tokens = response.usage_metadata.prompt_token_count
 response_tokens = response.usage_metadata.candidates_token_count
+
 
 print(response.text)
 print(f"Prompt Tokens: {prompt_tokens}")
